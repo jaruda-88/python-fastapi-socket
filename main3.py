a57f851170327f898b7f86684033b4eb22b76b3a
jaruda-88 <jaruda88@gmail.com>
2023-02-06 22:13:27 +0900

    update sample code

--- main3.py
+++ main3.py
@@ -13,57 +13,105 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  # can alter with time
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.mount("/static", StaticFiles(directory='static'), name='static')
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-template = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="templates")
 
-class WebSocketManager:
+
+class Notifier:
+    """
+        Manages chat room sessions and members along with message routing
+    """
 
     def __init__(self):
-        self.connectios: dict = defaultdict(dict)
-        self.generator = self.get_getnerator()
+        self.connections: dict = defaultdict(dict)
+        self.generator = self.get_notification_generator()
 
-    async def get_getnerator(self):
+    async def get_notification_generator(self):
         while True:
             message = yield
             msg = message["message"]
-            roomName = message["room_name"]
-            await self._ws(msg, roomName)
+            room_name = message["room_name"]
+            await self._notify(msg, room_name)
 
     def get_members(self, room_name):
         try:
-            return self.connectios[room_name]
+            return self.connections[room_name]
         except Exception:
             return None
 
     async def push(self, msg: str, room_name: str = None):
-        body = {"message": msg, "room_name": room_name}
-        await self.generator.asend(body)
+        message_body = {"message": msg, "room_name": room_name}
+        await self.generator.asend(message_body)
 
     async def connect(self, websocket: WebSocket, room_name: str):
         await websocket.accept()
-        if self.connectios[room_name] == {} or len(self.connectios[room_name]) == 0:
-            self.connectios[room_name] = []
-        self.connectios[room_name].append(websocket)
-        print(f"connected : {self.connectios[room_name]}")
+        if self.connections[room_name] == {} or len(self.connections[room_name]) == 0:
+            self.connections[room_name] = []
+        self.connections[room_name].append(websocket)
+        print(f"CONNECTIONS : {self.connections[room_name]}")
 
     def remove(self, websocket: WebSocket, room_name: str):
-        self.connectios[room_name].remove(websocket)
-        print(f"connection remove : {self.connectios[room_name]}")
+        self.connections[room_name].remove(websocket)
+        print(
+            f"CONNECTION REMOVED\nREMAINING CONNECTIONS : {self.connections[room_name]}"
+        )
 
-
-    async def _ws(self, message: str, room_name: str):
+    async def _notify(self, message: str, room_name: str):
         living_connections = []
-        while len(self.connectios[room_name]) > 0:
+        while len(self.connections[room_name]) > 0:
 
-            websocket = self.connectios[room_name].pop()
+            websocket = self.connections[room_name].pop()
             await websocket.send_text(message)
             living_connections.append(websocket)
-        self.connectios[room_name] = living_connections
+        self.connections[room_name] = living_connections
 
+
+notifier = Notifier()
+
+
+# controller routes
+@app.get("/{room_name}/{user_name}")
+async def get(request: Request, room_name, user_name):
+    return templates.TemplateResponse(
+        "chat_room.html",
+        {"request": request, "room_name": room_name, "user_name": user_name},
+    )
+
+
+@app.websocket("/ws/{room_name}")
+async def websocket_endpoint(
+    websocket: WebSocket, room_name, background_tasks: BackgroundTasks
+):
+    await notifier.connect(websocket, room_name)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            d = json.loads(data)
+            d["room_name"] = room_name
+
+            room_members = (
+                notifier.get_members(room_name)
+                if notifier.get_members(room_name) is not None
+                else []
+            )
+            if websocket not in room_members:
+                print("SENDER NOT IN ROOM MEMBERS: RECONNECTING")
+                await notifier.connect(websocket, room_name)
+
+            await notifier._notify(f"{data}", room_name)
+    except WebSocketDisconnect:
+        notifier.remove(websocket, room_name)
+
+def run():
+    import uvicorn
+    uvicorn.run(app)
+
+if __name__ == "__main__":
+    run()
