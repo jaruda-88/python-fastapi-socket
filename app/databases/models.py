8d86d5e3940195a47b5f4a76ae26c9ebfa3e1d77
jaruda-88 <jaruda88@gmail.com>
2023-02-22 23:07:27 +0900

    update

--- app/databases/models.py
+++ app/databases/models.py
@@ -4,6 +4,7 @@ from sqlalchemy import (
     Integer,
     String,
     DateTime,
+    ForeignKey,
     func
 )
 from sqlalchemy.orm import Session, relationship
@@ -19,5 +20,21 @@ class BaseModel:
 class Users(Base, BaseModel):
     __tablename__ =  "tb_users"
 
-    pw = Column(String(length=2000), nullable=True)
-    name = Column(String(length=255), nullable=True)
+    pw = Column(String(length=2000), nullable=False)
+    user_name = Column(String(length=255), nullable=False)
+    nick_name = Column(String(length=255), nullable=True)
+
+
+class Rooms(Base, BaseModel):
+    __tablename__ = "tb_rooms"
+
+    name = Column(String(length=255), nullable=False)
+
+
+class Members(Base, BaseModel):
+    __tablename__ = "tb_members"    
+
+    user_id = Column(Integer, ForeignKey('tb_users.id'))
+    room_id = Column(Integer, ForeignKey('tb_members.id'))
+
+
